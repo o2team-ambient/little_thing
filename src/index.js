8e3d6381ad0e7cb9a75aa7675d06bee5e934b997
Lee EC <lyxuncle@gmail.com>
2019-08-22 16:18:48 +0800

    feat: 更新 xview 上报

--- src/index.js
+++ src/index.js
@@ -20,6 +20,14 @@ try {
   console.log(e) 
 }
 
-Report.init({})
-Report.processPV()
+const handleReport = () => {
+  Report.init({})
+  Report.processPV()
+}
+
+if (typeof window.XView === 'undefined') {
+  handleReport()
+} else {
+  window.handleReport = handleReport
+}
   
